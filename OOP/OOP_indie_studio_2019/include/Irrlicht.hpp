4ef7f83b9299367405bc8a88c3ae522f4a0ca4a5
Sitpi Rajendran <39497470+SitpiRajendran@users.noreply.github.com>
2020-06-16 01:03:19 +0200

    Add files via upload

---
+++ OOP/OOP_indie_studio_2019/include/Irrlicht.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2020
+** IndieStudio
+** File description:
+** Irrlicht
+*/
+
+#ifndef __Irrlicht__include__
+#define __Irrlicht__include__
+
+#include <irrlicht.h>
+
+#define SUCCESS 0
+#define ERROR 84
+
+using namespace irr;
+
+using namespace io;
+using namespace gui;
+using namespace core;
+using namespace scene;
+using namespace video;
+
+using namespace std;
+
+#endif
